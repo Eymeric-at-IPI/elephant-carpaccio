cc6bb316bee6b9ab1829288d2abbb1f853332a0b
Eymeric Sertgoz <eymeric.sertgoz@gmail.com>
2021-01-06 08:59:40 +0100

    Sprint 1

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+
+
+def ask_state():
+    while True:
+        user_state = input("Choisissez l’état dans lequel vous êtes(Veuillez renseigner les deux lettres de votre état, parmi la liste suivante ? (UT - NV - TX - AL - CA)): ")
+        # if user_state in ["UT", "NV", "TX", "AL", "CA"]:
+        break
+
+    return user_state
+
+
+def start_text():
+    print("Vous venez de lancer le logiciel Elephant Carpaccio, celui-ci permet de calculer le montant d’une commande, en fonction du nombre de produit, du prix de ce même produit et enfin avoir une taxe différente en fonction de votre état.")
+    print("les états supporté par le logiciel sont les suivants : ")
+    print("UT - 6.85 %")
+    print("NV - 8.00 %")
+    print("TX - 6.25 %")
+    print("AL - 4.00 %")
+    print("CA - 8.25 %")
+
+
 def main():
     print("Hello world")
+    start_text()
+    user_state = ask_state()
 
 
 if __name__ == '__main__':
